4ad4f305d6db6611e7112e2aba58ec7c99b6681f
kevin.chen <ycpaladin@126.com>
2016-07-25 17:58:43 +0800

    调整数据流逻辑

--- src/reducers/items.js
+++ src/reducers/items.js
@@ -1,19 +1,51 @@
 import Immutable from 'immutable'
 import actionTypes from '../constants/actionTypes'
 
-const initialItems = Immutable.List([1, 2, 3]);
+const initialItems =
+    {
+        allComplete: false,
+        items: Immutable.List([
+            { text: 'kevin' },
+            { text: 'lxy' },
+            { text: 'ccw' }
+        ])
+    };
+
+
+function assign(item, newValue) {
+    return Object.assign({}, item, newValue);
+}
+
+
+function checkAllComplete(items) {
+    var result = items.filter((item, index) => item.complete == true);
+    return result.size == items.size
+}
 
 export default function items(state = initialItems, action) {
     switch (action.type) {
         case actionTypes.ADD_ITEM:
-            return state.push(action.value);
+            return assign(state, { items: state.items.push({ text: action.value }) })
         case actionTypes.DELETE_ITEM:
-            return state.delete(action.index)
-        case action.UPDATE_ITEM:
-            state[action.index] = action.newValue;
-            return state;
+            return assign(state, { items: state.items.delete(action.index) })
+        case actionTypes.UPDATE_ITEM:
+            return assign(state, { items: state.items.set(action.index, { text: action.newValue }) })
         case actionTypes.DELETE_ALL:
-            return state.clear();
+            return assign(state, { items: state.items.clear() });
+        case actionTypes.CHANGE_EDIT_STATE:
+            var item = assign(state.items.get(action.index), { isEditing: action.isEditing })
+            return assign(state, { items: state.items.set(action.index, item) })
+        case actionTypes.CURRENT_COMPLETED_STATE:
+            var item = assign(state.items.get(action.index), { complete: action.isCompleted })
+            var result = assign(state, { items: state.items.set(action.index, item) })
+            result.allComplete = checkAllComplete(result.items);
+            return result;
+        case actionTypes.SET_ALL_CHECKED:
+            var allItems = state.items.map((item, index) => assign(item, { complete: action.isCompleted }))
+            return {
+                allComplete: action.isCompleted,
+                items: allItems
+            };
         default:
             return state;
     }
